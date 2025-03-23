bf5107364d836d3d974b87c31e42af272a453ad6
spring712 <spring@mvpick.net>
2025-03-23 13:29:11 +0900

    2. 변수 - Var2

--- src/variable/Var1.java
+++ src/variable/Var1.java
@@ -1,5 +1,12 @@
 package variable;
 
+/**
+ * Var1
+ *
+ * @author Kwon Bomi / GitHub: aSpring712
+ * @since 2025-03-23
+ * @version 1.0
+ */
 public class Var1 {
 
 	public static void main(String[] args) {
---
+++ src/variable/Var2.java
@@ -0,0 +1,29 @@
+package variable;
+
+/**
+ * Var2
+ *
+ * @author Kwon Bomi / GitHub: aSpring712
+ * @since 2025-03-23
+ * @version 1.0
+ */
+public class Var2 {
+
+	public static void main(String[] args) {
+
+		int a; // 변수 선언 (정수)
+		a = 20; // 변수 초기화, 10 -> 20으로 변경
+		System.out.println(a); // 자바는 실행 시점에 변수의 값을 읽어서 사용한다. 변수의 값은 반복해서 읽을 수 있다.
+		System.out.println(a);
+		System.out.println(a);
+	}
+}
+
+/*
+* int a
+* 숫자 정수 (integer)를 보관할 수 있는 이름이 a라는 데이터 저장소를 만든다. -> 이것을 변수라 한다.
+* 이렇게 변수를 만드는 것을 변수 선언이라 한다.
+*
+* a = 10;
+* 선언한 변수에 처음으로 값을 대입해서 저장하는 것을 변수 초기화라 한다.
+* */

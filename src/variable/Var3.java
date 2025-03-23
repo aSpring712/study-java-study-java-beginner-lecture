82b2abd640df111280ae6d53f4017a956db28629
spring712 <spring@mvpick.net>
2025-03-23 13:31:40 +0900

    2. 변수 - Var3

---
+++ src/variable/Var3.java
@@ -0,0 +1,26 @@
+package variable;
+
+/**
+ * Var3
+ *
+ * @author Kwon Bomi / GitHub: aSpring712
+ * @since 2025-03-23
+ * @version 1.0
+ */
+public class Var3 {
+
+	public static void main(String[] args) {
+
+		int a; // 변수 선언
+		a = 10; // 변수 초기화: a(10)
+		System.out.println(a); // 10 출력
+
+		a = 50; // 변수 값 변경: a(10 -> 50)
+		System.out.println(a); // 50 출력
+	}
+}
+
+/*
+	변수: 이름 그대로 변할 수 있는 수
+	변수의 값을 변경하면 변수에 들어있던 기존 값은 삭제된다.
+* */

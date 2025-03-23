64188e5ecae562f2579a8a4e38b666affe9b1480
spring712 <spring@mvpick.net>
2025-03-23 13:40:08 +0900

    2. 변수 - Var6 변수는 초기화 해야한다

---
+++ src/variable/Var6.java
@@ -0,0 +1,21 @@
+package variable;
+
+/**
+ * Var6
+ *
+ * 변수는 초기화 해야한다
+ *
+ * @author Kwon Bomi / GitHub: aSpring712
+ * @since 2025-03-23
+ * @version 1.0
+ */
+public class Var6 {
+
+	public static void main(String[] args) {
+
+		// 지역 변수 (Local Variable)는 개발자가 직접 초기화를 해주어야 한다. (클래스 변수, 인스턴스 변수는 자바가 자동으로 초기화를 진행해준다.)
+		int a;
+//		System.out.println(a); // 컴파일 에러 발생 -> java: variable a might not have been initialized
+		// 컴파일 에러: 자바 문법에 맞지 않았을 때 발생하는 에러
+	}
+}

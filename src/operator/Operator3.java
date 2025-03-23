f220c08590a9de235c123de1f76fb0b07c1a3afc
spring712 <spring@mvpick.net>
2025-03-23 15:30:15 +0900

    3. 연산자 - 연산자 우선순위 +와 *

---
+++ src/operator/Operator3.java
@@ -0,0 +1,22 @@
+package operator;
+
+/**
+ * Operator3
+ *
+ * 연산자 우선순위
+ *
+ * @author Kwon Bomi / GitHub: aSpring712
+ * @since 2025-03-23
+ * @version 1.0
+ */
+public class Operator3 {
+
+	public static void main(String[] args) {
+
+		// 수학에서와 마찬가지로 자바에서도 덧셈보다 곱셉이 우선순위가 더 높고, 연산자 우선순위를 변경하려면 수학과 마찬가지로 괄호()를 사용하면 된다.
+		int sum1 = 1 + 2 * 3; // 1 + (2 * 3)과 같다.
+		int sum2 = (1 + 2) * 3;
+		System.out.println("sum1 = " + sum1); // sum1 = 7
+		System.out.println("sum2 = " + sum2); // sum2 = 9
+	}
+}

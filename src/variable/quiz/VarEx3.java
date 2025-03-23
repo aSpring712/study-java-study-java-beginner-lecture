0d9e71ae4609feca3cc726d515e2dfbb70d7e034
spring712 <spring@mvpick.net>
2025-03-23 14:57:07 +0900

    2. 변수 - 변수 관련 문제 및 풀이 1~3 추가

---
+++ src/variable/quiz/VarEx3.java
@@ -0,0 +1,27 @@
+package variable.quiz;
+
+/**
+ * VarEx3
+ *
+ * @author Kwon Bomi / GitHub: aSpring712
+ * @since 2025-03-23
+ * @version 1.0
+ */
+public class VarEx3 {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Q3. long, boolean 데이터 타입
+		 *
+		 * long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 초기화한 후 출력하는 프로그램을 작성하세요.
+		 * boolean 타입의 변수를 선언하고, 그 변수를 true로 초기화한 후 출력하는 프로그램을 작성하세요.
+		 * */
+
+		long longVar = 10000000000L;
+		System.out.println(longVar);
+
+		boolean booleanVar = true;
+		System.out.println(booleanVar);
+	}
+}

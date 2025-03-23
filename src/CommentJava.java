52179583d21992ed713813d6ded9a528dee7a294
spring712 <spring@mvpick.net>
2025-03-23 10:59:53 +0900

    주석(comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,18 @@
+public class CommentJava {
+
+	// psvm + enter
+	public static void main(String[] args) {
+
+		// sout + enter
+		System.out.println("hello java1"); // hello java1을 출력합니다. -> 한 줄 주석 (single line comment)
+//		System.out.println("hello java2");
+
+		// 여러 줄(범위) 주석 (multi line comment)
+		/*
+		System.out.println("hello java3");
+		System.out.println("hello java4");
+		*/
+	}
+}
+
+// 주석은 자바 프로그램이 읽지 않고 무시하는 부분으로 사람이 읽기 위해서 사용한다.

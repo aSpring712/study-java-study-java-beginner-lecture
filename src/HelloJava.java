773b60f844cf51b10f54bc8bc84235ac3263bc32
spring712 <spring@mvpick.net>
2025-03-23 10:53:29 +0900

    자바 프로그램 실행 - hello java

---
+++ src/HelloJava.java
@@ -0,0 +1,13 @@
+// 파일 이름 (HelloJava)와 클래스명이 같아야 함
+public class HelloJava { // HelloJava 클래스 범위 시작
+
+	// 프로그램이 시작되는 시작점 (프로그램 실행 시 시작 시점인 main을 찾는다)
+	// 메서드
+	public static void main(String[] args) {
+		System.out.println("hello java"); // 값을 콘솔에 출력하는 기능, java는 문장이 끝날 때 ';'으로 끝남
+	} // main() 메서드 범위 끝
+} // {} 블록(block)은 클래스의 시작과 끝을 나타낸다
+
+/*
+* 클래스 안에는 메서드가 있고 메서드 안에는 코드가 있다
+* */

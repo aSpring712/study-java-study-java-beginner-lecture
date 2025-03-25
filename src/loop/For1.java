7eae70e407db12a2415f3b63649d3cc35ab8c1c2
spring712 <spring@mvpick.net>
2025-03-25 16:21:19 +0900

    5. 반복문 - for문1.md -> for문.md로 변경

---
+++ src/loop/For1.java
@@ -0,0 +1,18 @@
+package loop;
+
+/**
+ * For1
+ *
+ * @author Kwon Bomi / GitHub: aSpring712
+ * @since 2025-03-25
+ * @version 1.0
+ */
+public class For1 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
